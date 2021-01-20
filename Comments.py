c497759a88c28f6418d9e22349239d71093e4e91
Kathryn So <77600634+Kathrynms@users.noreply.github.com>
2021-01-19 21:40:20 -0700

    Comments
    Follow along practice with YouTube video Learn Python - Full Course for Beginners by FreeCodeCamp.org
    
    Going over how to comment with code and uses for comments. Comments will not show up when running the program.

---
+++ Comments.py
@@ -0,0 +1,12 @@
+
+'''
+Another way to make comments
+'''
+
+# super python official
+# This prints out a string
+# print("Comments are fun")
+# Also used for commenting out code
+
+print("Comments are fun")
+
